d60d5fea0acccebe3afcf60b36d5215d25460dd2
abdudsoul <abdudsoul@gmail.com>
2023-08-06 20:53:45 +0400

    temporary fix for loading chat of a model not in the default list

--- src/App.tsx
+++ src/App.tsx
@@ -22,83 +22,84 @@ function App()
     {
       alert("Failed getting model data from OpenRouterAI API.\nTry restarting or refreshing.\n\n(Currently default models will be shown that may include deprecated model or invalid information.)")
     }
-  }
-  initalizeModelData();
 
-  const initialiseNewChat = useInitialiseNewChat();
-  const setChats = useStore((state) => state.setChats);
-  const setTheme = useStore((state) => state.setTheme);
-  const setApiKey = useStore((state) => state.setApiKey);
-  const setCurrentChatIndex = useStore((state) => state.setCurrentChatIndex);
+    const initialiseNewChat = useInitialiseNewChat();
+    const setChats = useStore((state) => state.setChats);
+    const setTheme = useStore((state) => state.setTheme);
+    const setApiKey = useStore((state) => state.setApiKey);
+    const setCurrentChatIndex = useStore((state) => state.setCurrentChatIndex);
 
-  useEffect(() =>
-  {
-    document.documentElement.lang = i18n.language;
-    i18n.on('languageChanged', (lng) =>
+    useEffect(() =>
     {
-      document.documentElement.lang = lng;
-    });
-  }, []);
-
-  useEffect(() =>
-  {
-    // legacy local storage
-    const oldChats = localStorage.getItem('chats');
-    const apiKey = localStorage.getItem('apiKey');
-    const theme = localStorage.getItem('theme');
-
-    if (apiKey)
-    {
-      // legacy local storage
-      setApiKey(apiKey);
-      localStorage.removeItem('apiKey');
-    }
-
-    if (theme)
-    {
-      // legacy local storage
-      setTheme(theme as Theme);
-      localStorage.removeItem('theme');
-    }
-
-    if (oldChats)
-    {
-      // legacy local storage
-      try
+      document.documentElement.lang = i18n.language;
+      i18n.on('languageChanged', (lng) =>
       {
-        const chats: ChatInterface[] = JSON.parse(oldChats);
-        if (chats.length > 0)
+        document.documentElement.lang = lng;
+      });
+    }, []);
+
+    useEffect(() =>
+    {
+      // legacy local storage
+      const oldChats = localStorage.getItem('chats');
+      const apiKey = localStorage.getItem('apiKey');
+      const theme = localStorage.getItem('theme');
+
+      if (apiKey)
+      {
+        // legacy local storage
+        setApiKey(apiKey);
+        localStorage.removeItem('apiKey');
+      }
+
+      if (theme)
+      {
+        // legacy local storage
+        setTheme(theme as Theme);
+        localStorage.removeItem('theme');
+      }
+
+      if (oldChats)
+      {
+        // legacy local storage
+        try
         {
-          setChats(chats);
-          setCurrentChatIndex(0);
-        } else
+          const chats: ChatInterface[] = JSON.parse(oldChats);
+          if (chats.length > 0)
+          {
+            setChats(chats);
+            setCurrentChatIndex(0);
+          }
+          else
+          {
+            initialiseNewChat();
+          }
+        } catch (e: unknown)
+        {
+          console.log(e);
+          initialiseNewChat();
+        }
+        localStorage.removeItem('chats');
+      } else
+      {
+        // existing local storage
+        const chats = useStore.getState().chats;
+        const currentChatIndex = useStore.getState().currentChatIndex;
+        if (!chats || chats.length === 0)
         {
           initialiseNewChat();
         }
-      } catch (e: unknown)
-      {
-        console.log(e);
-        initialiseNewChat();
+        if (
+          chats &&
+          !(currentChatIndex >= 0 && currentChatIndex < chats.length)
+        )
+        {
+          setCurrentChatIndex(0);
+        }
       }
-      localStorage.removeItem('chats');
-    } else
-    {
-      // existing local storage
-      const chats = useStore.getState().chats;
-      const currentChatIndex = useStore.getState().currentChatIndex;
-      if (!chats || chats.length === 0)
-      {
-        initialiseNewChat();
-      }
-      if (
-        chats &&
-        !(currentChatIndex >= 0 && currentChatIndex < chats.length)
-      )
-      {
-        setCurrentChatIndex(0);
-      }
-    }
-  }, []);
+    }, []);
+  }
+  initalizeModelData();
 
   return (
     <div className='overflow-hidden w-full h-full relative'>
